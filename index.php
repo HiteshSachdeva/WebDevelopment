fdfdd4e47bbc60a7574eb3a6393d3d4b8f5e2df0
hitesh3645 <69100443+hitesh3645@users.noreply.github.com>
2021-01-28 00:16:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>WEB DEVELOPMENT</title>
+<body>
+<h1>welcome to new semester</h1>
+<h2>welcome to new semester</h2>
+<h3>welcome to new semester</h3>
+</body>
+</head>
+</html>
